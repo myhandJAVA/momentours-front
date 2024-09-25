58572dff98a6cd15b89d93f14312cf64d3afb874
yuhyejin <hj_eu@naver.com>
2024-09-25 23:57:28 +0900

    feat: 공통 버튼 핑크, 갈색, 연한갈색 버튼 컴포넌트  추가

---
+++ momentours-front/src/components/common/CommonBrownButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="common-button" @click="handleClick">
+        {{ label }}
+    </button>
+</template>
+
+<script setup>
+    const props = defineProps({
+        label: {
+        type: String,
+        required: true
+    },
+    onClick: {
+        type: Function,
+        default: () => {}
+    }
+    });
+
+    const handleClick = () => {
+        props.onClick();
+    };
+</script>
+
+<style scoped>
+    .common-button {
+        background-color: #C5B697;
+        color: #333;
+        border: none;
+        padding: 7px 28px;
+        border-radius: 7px;
+        font-size: 15px;
+        cursor: pointer;
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    }
+</style>
