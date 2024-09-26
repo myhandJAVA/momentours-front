c685aff92d9e454a2678bf3a738508a2294a4b4a
mingeun <caolian2003@naver.com>
2024-09-26 16:19:09 +0900

    feat: 수정사항 반영

--- momentours-front/src/App.vue
+++ momentours-front/src/App.vue
@@ -3,13 +3,7 @@
     <Header/>
   </header>
   <main>
-    <div class="dd">
-      <!-- 업로드 버튼 -->
-      <CommonPinkButton label="업로드"/>
-      <CommonBrownButton label="갈색버튼"/>
-      <CommonLightBrownButton label="연한갈색버튼"/>
       <MainPage/>
-    </div>
   </main>
   <footer>
     <Footer/>  
@@ -19,9 +13,7 @@
 <script setup>
   import Footer from './components/common/footer.vue';
   import Header from './components/common/Header.vue';
-  import CommonPinkButton from './components/common/CommonPinkButton.vue';
-  import CommonBrownButton from './components/common/CommonBrownButton.vue';
-  import CommonLightBrownButton from './components/common/CommonLightBrownButton.vue';
+
   import MainPage from './views/HomeView.vue';
 </script>
 
