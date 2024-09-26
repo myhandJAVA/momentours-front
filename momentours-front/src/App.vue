38209de47556520a96d7329f406eaa09f5371cb9
mingeun <caolian2003@naver.com>
2024-09-26 15:48:12 +0900

    feat: 메인 페이지 html및 css 기능 개발

--- momentours-front/src/App.vue
+++ momentours-front/src/App.vue
@@ -3,12 +3,7 @@
     <Header/>
   </header>
   <main>
-    <div class="dd">
-      <!-- 업로드 버튼 -->
-      <CommonPinkButton label="업로드"/>
-      <CommonBrownButton label="갈색버튼"/>
-      <CommonLightBrownButton label="연한갈색버튼"/>
-    </div>
+    <MainPage/>
   </main>
   <footer>
     <Footer/>  
@@ -18,14 +13,8 @@
 <script setup>
   import Footer from './components/common/footer.vue';
   import Header from './components/common/Header.vue';
-  import CommonPinkButton from './components/common/CommonPinkButton.vue';
-  import CommonBrownButton from './components/common/CommonBrownButton.vue';
-  import CommonLightBrownButton from './components/common/CommonLightBrownButton.vue';
+  import MainPage from './views/HomeView.vue';
 </script>
 
 <style scoped>
-  .dd {
-    margin-top: 10px;
-    height: 50px;
-  }
 </style>
