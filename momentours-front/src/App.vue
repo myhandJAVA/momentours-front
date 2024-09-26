5f2d4e7926ccdf65f7ca230f653b379f453398a5
Vorschlag-bit <https://github.com/Vorschlag-bit/testR.git>
2024-09-26 13:45:49 +0900

    feat: sidebar 개발

--- momentours-front/src/App.vue
+++ momentours-front/src/App.vue
@@ -1,51 +1,40 @@
 <template>
-  <div class="app-flex-container">
-  <header class="header">
-    <Header/>
-    <nav style="text-align: center;">
-        <tr>
-          <td><RouterLink class="link-button" to="/couple" replace>couple</RouterLink></td>
-          <td><RouterLink class="link-button" to="/diary" replace>diary</RouterLink></td>
-          <td><RouterLink class="link-button" to="/moment" replace>moment</RouterLink></td>
-          <td><RouterLink class="link-button" to="/momentcourse" replace>momentcourse</RouterLink></td>
-          <td><RouterLink class="link-button" to="/randomquestion" replace>randomquestion</RouterLink></td>
-          <td><RouterLink class="link-button" to="/schedule" replace>schedule</RouterLink></td>
-          <td><RouterLink class="link-button" to="/todocourse" replace>todocourse</RouterLink></td>
-          <td><RouterLink class="link-button" to="/user" replace>user</RouterLink></td>
-        </tr>
-    </nav>
-  </header>
-  <main class="main">
-        <RouterView/>
-  </main>
-  <footer class="footer">
-    <Footer/>  
-  </footer>
+  <div class="app-container">
+    <Header @toggle-sidebar="toggleSidebar" />
+    <Sidebar :is-active="isSidebarActive" @close="closeSidebar" />
+      <main>
+        <!-- 메인 콘텐츠가 여기에 들어갑니다 -->
+      </main>
+    <Footer />
   </div>
 </template>
 
 <script setup>
-  import Footer from './components/common/footer.vue';
-  import Header from './components/common/Header.vue';
+import { ref } from 'vue';
+import Footer from './components/common/Footer.vue';
+import Header from './components/common/Header.vue';
+import Sidebar from './components/common/Sidebar.vue';
+
+const isSidebarActive = ref(false);
+
+const toggleSidebar = () => {
+  isSidebarActive.value = !isSidebarActive.value;
+};
+
+const closeSidebar = () => {
+  isSidebarActive.value = false;
+};
 </script>
 
 <style scoped>
-  .app-flex-container{
-    display: flex;
-    flex-direction: column;
-    align-items: normal;
-    justify-content: space-evenly;
-  }
-  .main{
-    height:90vh;
-  }
-  .link-button {
-  display: inline-block;
-  padding: 10px 20px;
-  background-color: #f2f2f2;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  text-decoration: none;
-  color: #333;
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+
+main {
+  flex: 1;
 }
 </style>
---
+++ momentours-front/src/components/common/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sidebar-overlay" v-if="isActive" @click="closeSidebar">
+    <div class="sidebar" @click.stop>
+      <div class="logo-container">
+    <div class="momentours-logo">
+      <img src="@/assets/icons/momentours-logo.svg" alt="logo">
+    </div>
+    <div class="momentours">Momentours</div>
+  </div>
+  <div class="menu-container">
+    <div class="sidebar_divider"></div>
+    <div class="menu-item2">홈</div>
+    <div class="sidebar_divider"></div>
+    <div class="menu-item">마이페이지</div>
+    <div class="menu-item2">커플페이지</div>
+    <div class="sidebar_divider"></div>
+    <div class="pages">PAGES</div>
+    <div class="menu-item">추억</div>
+    <div class="menu-item">추억코스</div>
+    <div class="menu-item">예정데이트코스</div>
+    <div class="menu-item">일기</div>
+    <div class="menu-item">랜덤질문</div>
+    <div class="menu-item2">일정</div>
+    <div class="sidebar_divider"></div>
+    <div class="menu-item">공지</div>
+    <div class="menu-item">로그아웃</div>
+  </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps(['isActive']);
+const emit = defineEmits(['close']);
+
+const closeSidebar = () => {
+  emit('close');
+};
+</script>
+
+<style src="@/assets/css/side.css"></style>
